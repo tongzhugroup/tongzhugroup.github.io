5ac8fd8578fd94722414a895648037ea2364e08a
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2021-02-28 16:21:19 -0500

    chore: remove min.js replacer

--- scripts/jsdelivr_js.js
+++ scripts/jsdelivr_js.js
@@ -6,25 +6,6 @@ hexo.extend.filter.register('after_generate', () => {
     hexo.route.remove('js/utils.js');
   });
 
-function replace_url(str, old_url, new_url){
-  old_url = new RegExp(old_url.split('/').join('\\/'), "g");
-  return str.replace(old_url, new_url);
-}
-
-// .js -> .min.js
-function use_min_js(str, package, old_path, new_path){
-  var old_url = `//cdn.jsdelivr.net/npm/${package}@(([\\s\\S])*?)/${old_path}`;
-  var new_url = `//cdn.jsdelivr.net/npm/${package}@$1/${new_path}`;
-  return replace_url(str, old_url, new_url);
-}
-
-hexo.extend.filter.register('after_render:html', function(str, data){
-  str = use_min_js(str, 'hexo-theme-next', 'source/js/next-boot.js', 'source/js/next-boot.min.js');
-  str = use_min_js(str, 'hexo-theme-next', 'source/js/utils.js', 'source/js/utils.min.js');
-  str = use_min_js(str, 'hexo-theme-next', 'source/js/schemes/muse.js', 'source/js/schemes/muse.min.js');
-  return str;
-});
-
 hexo.extend.injector.register('body_end', () => {
 	return js("https://cdn.jsdelivr.net/gh/njzjz/gist@5eab1974597adee1a1f5de13cc8acd53c14d6ba1/modernizr-custom.js");
 });
