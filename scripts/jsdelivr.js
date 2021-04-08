dfb90ece6301f9d25fee316fc6646df52a6b6e13
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2021-04-07 22:49:23 -0400

    chore: refactor icons (#52)

--- scripts/jsdelivr.js
+++ scripts/jsdelivr.js
@@ -1,14 +1,23 @@
 const js = hexo.extend.helper.get('js').bind(hexo);
 const crypto = require('crypto');
 const { npm_url, gh_url } = require("jsdelivr_url");
-const { version } = require("hexo-theme-next/package.json");
+const { name: next_name, version: next_version } = require("hexo-theme-next/package.json");
+const { name: icon_name, version: icon_version } = require("@njzjz/icons/package.json");
 
 hexo.on('generateBefore', function () {
-  hexo.theme.config.images = npm_url("hexo-theme-next", version, "source/images");
+  hexo.theme.config.images = npm_url(next_name, next_version, "source/images");
   if (process.env.CSS_COMMIT) {
     hexo.config.assets_prefix = gh_url("tongzhugroup", "tongzhugroup.github.io", process.env.CSS_COMMIT, "");
     hexo.theme.config.css = gh_url("tongzhugroup", "tongzhugroup.github.io", process.env.CSS_COMMIT, "css");
   }
+  // icons
+  const avatar_url = npm_url(icon_name, icon_version, "computchem/logo.jpg");
+  hexo.theme.config.avatar.url = avatar_url;
+  hexo.theme.config.favicon.small = avatar_url;
+  hexo.theme.config.favicon.medium = avatar_url;
+  hexo.theme.config.favicon.apple_touch_icon = avatar_url;
+  hexo.theme.config.favicon.safari_pinned_tab = avatar_url;
+  hexo.theme.config.icons = npm_url(icon_name, icon_version, "");
 });
 
 hexo.extend.filter.register('after_generate', () => {
